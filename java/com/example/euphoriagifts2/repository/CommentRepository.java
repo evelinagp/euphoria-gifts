76e498ffc316e744be8bab20c5f2ed1ee19575ff
Evelina Pancheva <81183303+evitapk@users.noreply.github.com>
2022-08-03 15:35:56 +0300

    Delete java/com/example/euphoriagifts2 directory

--- java/com/example/euphoriagifts2/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.euphoriagifts2.repository;
-
-import com.example.euphoriagifts2.model.entity.CommentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
-}
