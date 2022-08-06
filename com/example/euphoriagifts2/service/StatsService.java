de51d6c2cd04778a9a8307b13bec30d1e5150411
Evelina Pancheva <81183303+evitapk@users.noreply.github.com>
2022-08-06 23:36:21 +0300

    Delete com/example/euphoriagifts2 directory

--- com/example/euphoriagifts2/service/StatsService.java
+++
@@ -1,8 +0,0 @@
-package com.example.euphoriagifts2.service;
-
-import com.example.euphoriagifts2.model.view.StatsView;
-
-public interface StatsService {
-    void onRequest();
-    StatsView getStats();
-}
